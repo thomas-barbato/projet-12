483d6730122b1e46e62be9851ff030f11891baa0
Case Gibson <t.barbato78@gmail.com>
2023-06-29 15:43:21 +0200

    fix: settings , urls

--- urls.py
+++ urls.py
@@ -3,11 +3,7 @@ from django.urls import path, include
 from api import urls as api_urls
 
 
-admin_pattern = [
-    path("admin/", admin.site.urls),
-]
-
 urlpatterns = [
-    path('', include((admin_pattern, 'admin'), namespace="backend")),
+    path("admin/", admin.site.urls),
     path("api/", include(api_urls)),
 ]
